568911eac0fe61fe1583db0d303dbfee8c0721f4
Kinnari7 <kinnari.ldrpit@gmail.com>
2022-04-26 10:04:49 +0530

    fix: add readme file

--- project1/src/Button.js
+++ project1/src/Button.js
@@ -3,12 +3,17 @@ import React from 'react';
 const Button = () => {
 
     const clickButton = () => {
-        console.log('clicked')
-    }
+        console.log('clicked');
+    };
 
     return (
-        <button onClick={clickButton}>MFE1 Button</button>
-    )
+        <div>
+            <h1 style={{ marginBottom: '10px' }}>Project 1</h1>
+            <h2>Microfrontend</h2>
+            <h4>Project 2 call project 1 component.</h4>
+            <button onClick={clickButton}>Button</button>
+        </div>
+    );
 };
 
 export default Button;
